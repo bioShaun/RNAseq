d9650512a7b8f210c885f5adf4e3026c5fcd2f4c
BioShuan <ricekent@163.com>
2017-08-10 10:45:41 +0800

    add dispersion for non-replicates analysis

--- R/quantification/diff_analysis_v2.R
+++ R/quantification/diff_analysis_v2.R
@@ -20,6 +20,7 @@ p <- add_argument(p, '--gene2tr',    help = 'gene id and transcript id mapping f
 p <- add_argument(p, '--out_dir', help = 'output directory')
 p <- add_argument(p, '--qvalue',     help = 'diff gene qvalue cutoff', default = 0.05)
 p <- add_argument(p, '--logfc',      help = 'diff gene logfc cutoff',  default = 1)
+p <- add_argument(p, '--dispersion', help = 'for analysis with no replicates',  default = 0.1)
 argv <- parse_args(p)
 
 # test
@@ -39,6 +40,7 @@ gene2tr_file <- argv$gene2tr
 outdir <- argv$out_dir
 qvalue <- argv$qvalue
 logfc <- argv$logfc
+dispersion <- argv$dispersion
 
 dir.create(outdir, showWarnings = FALSE)
 samples <- read.delim(sample_inf, stringsAsFactors = F, header = F)
@@ -69,9 +71,15 @@ o <- log(calcNormFactors(each_pair_cts/normMat)) + log(colSums(each_pair_cts/nor
 conditions = factor(c(rep(each_pair[1], length(con1_sample)), rep(each_pair[2], length(con2_sample))))
 y <- DGEList(each_pair_cts, group=conditions)
 y$offset <- t(t(log(normMat)) + o)
-y <- estimateDisp(y)
+
 ## diff
-et <- exactTest(y, pair=rev(each_pair))
+if (length(con1_sample) > 1 && length(con2_sample) > 1) {
+  y <- estimateDisp(y)
+  et <- exactTest(y, pair=rev(each_pair))
+} else {
+  et <- exactTest(y, pair=rev(each_pair), dispersion=dispersion)
+}
+
 tTags <- topTags(et,n=NULL)
 new_tTags <- tTags$table
 new_tTags <- new_tTags[, !(names(new_tTags) %in% c("logCPM"))]
